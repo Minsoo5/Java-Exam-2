bf10ba841434ec5b2aac67e78bfff1c7c4b4c400
Minsoo Jo <134647176+Minsoo5@users.noreply.github.com>
2023-07-24 09:52:35 -0400

    Some strings done

--- src/main/java/rocks/zipcode/assessment2/fundamentals/BasicStringUtils.java
+++ src/main/java/rocks/zipcode/assessment2/fundamentals/BasicStringUtils.java
@@ -7,7 +7,7 @@ public class BasicStringUtils {
      * @return concatenation of `string1` and `string2`
      */
     public static String concatentate(String string1, String string2) {
-        return null;
+        return string1 + string2;
     }
 
     /**
@@ -15,7 +15,12 @@ public class BasicStringUtils {
      * @return an identical string with characters in reverse order
      */
     public static String reverse(String string1) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = string1.length()-1; i >=0; i--) {
+            sb.append(string1.charAt(i));
+        }
+
+        return sb.toString();
     }
 
     /**
@@ -24,7 +29,7 @@ public class BasicStringUtils {
      * @return concatenation of the reverse of `string1` and reverse of `string2`
      */
     public static String reverseThenConcatenate(String string1, String string2) {
-        return null;
+        return reverse(string1) + reverse(string2);
     }
 
     /**
@@ -33,7 +38,19 @@ public class BasicStringUtils {
      * @return `string` with `charactersToRemove` removed
      */
     public static String removeCharacters(String string, String charactersToRemove) {
-        return null;
+        // Hammner
+        // am
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < string.length(); i++) {
+            for (int j = 0; j < charactersToRemove.length(); j++) {
+                if (string.charAt(i) == charactersToRemove.charAt(j)) {
+
+                }
+            }
+        }
+
+        return string;
     }
 
     /**
